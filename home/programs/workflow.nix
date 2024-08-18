51e7d9771c788b12a477ad6a12da5c02dd60a86f
L-Trump <ltrump@163.com>
2024-08-18 15:04:20 +0800

    Fix onedriver systemd service

--- home/programs/workflow.nix
+++ home/programs/workflow.nix
@@ -25,16 +25,16 @@
 
   services.syncthing.enable = true;
 
-  # systemd.user.services."onedriver@" = {
-  #   Unit.Description = "onedriver";
-  #   Service = {
-  #     ExecStart = "${pkgs.onedriver}/bin/onedriver %f";
-  #     ExecStopPost = "/run/wrappers/bin/fusermount -uz /%I";
-  #     Restart = "on-abnormal";
-  #     RestartSec = 3;
-  #     RestartForceExitStatus = 2;
-  #     Environment = "PATH=/run/current-system/sw/bin";
-  #   };
-  #   Install.WantedBy = [ "default.target" ];
-  # };
+  systemd.user.services."onedriver@" = {
+    Unit.Description = "onedriver";
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:/run/current-system/sw/bin:/etc/profiles/per-user/%u/bin";
+      ExecStart = "${pkgs.onedriver}/bin/onedriver %f";
+      ExecStopPost = "/run/wrappers/bin/fusermount -uz /%I";
+      Restart = "on-abnormal";
+      RestartSec = 3;
+      RestartForceExitStatus = 2;
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
 }
