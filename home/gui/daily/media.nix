6c0b08051d52d041e4897ef662841f6f952e5acf
L-Trump <ltrump@163.com>
2024-09-12 14:31:22 +0800

    OBS: remove unused plugins

--- home/gui/daily/media.nix
+++ home/gui/daily/media.nix
@@ -37,23 +37,23 @@
         obs-nvfbc
         obs-teleport
         # obs-hyperion
-        droidcam-obs
-        obs-vkcapture
-        obs-gstreamer
-        obs-3d-effect
-        input-overlay
-        obs-multi-rtmp
-        obs-source-clone
-        obs-shaderfilter
-        obs-source-record
-        obs-livesplit-one
-        looking-glass-obs
-        obs-vintage-filter
-        obs-command-source
-        obs-move-transition
-        obs-backgroundremoval
-        advanced-scene-switcher
-        obs-pipewire-audio-capture
+        # droidcam-obs
+        # obs-vkcapture
+        # obs-gstreamer
+        # obs-3d-effect
+        # input-overlay
+        # obs-multi-rtmp
+        # obs-source-clone
+        # obs-shaderfilter
+        # obs-source-record
+        # obs-livesplit-one
+        # looking-glass-obs
+        # obs-vintage-filter
+        # obs-command-source
+        # obs-move-transition
+        # obs-backgroundremoval
+        # advanced-scene-switcher
+        # obs-pipewire-audio-capture
       ];
     };
   };
