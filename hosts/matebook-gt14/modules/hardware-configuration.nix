ea7ed8569a5b26e26f7ed929a7586fde101d1341
L-Trump <ltrump@163.com>
2024-09-14 17:06:58 +0800

    Host: matebook-gt14 add btrfs root pool

--- hosts/matebook-gt14/modules/hardware-configuration.nix
+++ hosts/matebook-gt14/modules/hardware-configuration.nix
@@ -12,10 +12,23 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
+  boot.extraModprobeConfig = "options kvm_intel nested=1"; # for intel cpu
   boot.extraModulePackages = [];
+
+  # supported file systems, so we can mount any removable disks with these filesystems
+  boot.supportedFilesystems = [
+    "ext4"
+    "btrfs"
+    "xfs"
+    "ntfs"
+    "fat"
+    "vfat"
+    "exfat"
+  ];
+
   boot.loader.grub = {
     enable = true;
     device = "nodev";
@@ -37,6 +50,14 @@
   ];
 
   hardware.firmware = with pkgs; [sof-firmware];
+
+  fileSystems."/btr_pool" = {
+    device = "/dev/disk/by-uuid/bc5fbdc7-9582-4c3f-8afd-9c340e3e9ec5";
+    fsType = "btrfs";
+    # btrfs's top-level subvolume, internally has an id 5
+    # we can access all other subvolumes from this subvolume.
+    options = ["subvolid=5"];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/bc5fbdc7-9582-4c3f-8afd-9c340e3e9ec5";
