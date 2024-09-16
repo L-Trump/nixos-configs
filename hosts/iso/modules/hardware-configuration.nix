71d45d8d2f680e5c04380394b4b71448d542ff2b
L-Trump <ltrump@163.com>
2024-09-16 23:30:26 +0800

    Hosts: iso use latest kernel; n100 disable intel xe driver

--- hosts/iso/modules/hardware-configuration.nix
+++ hosts/iso/modules/hardware-configuration.nix
@@ -40,7 +40,7 @@
     efiSysMountPoint = "/boot/efi";
   };
   # Use LTS kernel
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- hosts/n100/modules/core.nix
+++ hosts/n100/modules/core.nix
@@ -41,7 +41,7 @@ in {
     vpl-gpu-rt
   ];
 
-  hardware.intelgpu.driver = "xe";
+  # hardware.intelgpu.driver = "xe";
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
