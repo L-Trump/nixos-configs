79767e23e7d7da43189bbbc836fdc17c6924c2c9
L-Trump <ltrump@163.com>
2024-09-21 16:29:42 +0800

    VPN: secrets optional enable

--- secrets/vpn.nix
+++ secrets/vpn.nix
@@ -6,13 +6,13 @@
 }: let
   inherit (inputs) mysecrets;
 in {
-  age.secrets.ipsec-conf = {
+  age.secrets.ipsec-conf = lib.mkIf config.mymodules.desktop.enable {
     file = "${mysecrets}/vpn/ipsec.conf.age";
     path = "/etc/ipsec.conf";
   };
-  age.secrets.ipsec-secrets = {
+  age.secrets.ipsec-secrets = lib.mkIf config.mymodules.desktop.enable {
     file = "${mysecrets}/vpn/ipsec.secrets.age";
     path = "/etc/ipsec.secrets";
   };
-  age.secrets.swanctl-conf.file = "${mysecrets}/vpn/strongswan.conf.age";
+  age.secrets.swanctl-conf.file = lib.mkIf config.mymodules.desktop.enable "${mysecrets}/vpn/strongswan.conf.age";
 }
