53c1a96518c0b95c862db28e2cb93e87d7d13c5c
L-Trump <ltrump@163.com>
2024-09-27 22:30:22 +0800

    Update flake.nix; remove custom easytier

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,6 @@
 {pkgs ? import <nixpkgs> {}, ...}: let
   inherit (pkgs) lib libsForQt5;
 in {
-  easytier = pkgs.callPackage ./easytier {};
   xdg-desktop-portal-termfilechooser = pkgs.callPackage ./xdg-desktop-portal-termfilechooser {};
   ipu6-camera-hal = pkgs.callPackage ./ipu6-camera-hal {};
   ipu6epmtl-camera-hal = pkgs.callPackage ./ipu6-camera-hal {ipuVersion = "ipu6epmtl";};
