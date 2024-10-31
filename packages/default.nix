e76a9ef9b906c334634cc3b712d071694ce9443f
L-Trump <ltrump@163.com>
2024-10-31 21:06:29 +0800

    feat(package): clouddrive sync to upstream

--- packages/default.nix
+++ packages/default.nix
@@ -9,6 +9,6 @@ in {
   wpsoffice-cn = libsForQt5.callPackage ./wpsoffice-cn {};
   # openvswitch = pkgs-stable.openvswitch.override {kernel = null;};
   # easytier = pkgs.callPackage ./easytier {};
-  clouddrive2 = pkgs.callPackage ./clouddrive2 {};
+  # clouddrive2 = pkgs.callPackage ./clouddrive2 {};
   # snipaste = pkgs.callPackage ./snipaste {};
 }
