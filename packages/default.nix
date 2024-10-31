af1165f6a96ceb8608c927b4932f56e87a09af8d
L-Trump <ltrump@163.com>
2024-10-31 11:11:49 +0800

    fix(packages): openvswitch sync to upstream

--- packages/default.nix
+++ packages/default.nix
@@ -7,7 +7,7 @@
 in {
   xdg-desktop-portal-termfilechooser = pkgs.callPackage ./xdg-desktop-portal-termfilechooser {};
   wpsoffice-cn = libsForQt5.callPackage ./wpsoffice-cn {};
-  openvswitch = pkgs-stable.openvswitch.override {kernel = null;}; # wait for mandb fix TODO
+  # openvswitch = pkgs-stable.openvswitch.override {kernel = null;};
   # easytier = pkgs.callPackage ./easytier {};
   clouddrive2 = pkgs.callPackage ./clouddrive2 {};
   # snipaste = pkgs.callPackage ./snipaste {};
