0ce4ac455b324752d54a39b01cec81cdedb97587
L-Trump <ltrump@163.com>
2024-11-07 16:12:56 +0800

    fix(hyprland): fix decoration:shadow options

--- home/gui/hyprland/hyprland-conf.nix
+++ home/gui/hyprland/hyprland-conf.nix
@@ -63,10 +63,12 @@
       # Change transparency of focused and unfocused windows
       active_opacity = 1.0;
       inactive_opacity = 0.8;
-      drop_shadow = true;
-      shadow_range = 4;
-      shadow_render_power = 3;
-      "col.shadow" = "rgba(1a1a1aee)";
+      shadow = {
+        enabled = true;
+        # "col.shadow" = "rgba(1a1a1aee)";
+        render_power = 3;
+        range = 4;
+      };
       # https://wiki.hyprland.org/Configuring/Variables/#blur
       blur = {
         enabled = true;
