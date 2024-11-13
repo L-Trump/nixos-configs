51062d7f3fa3ebd9f8644c20583b0ebe10c33ab5
L-Trump <ltrump@163.com>
2024-11-13 22:12:55 +0800

    feat(gui): mpv add practical scripts and keybindings

--- home/gui/media.nix
+++ home/gui/media.nix
@@ -31,7 +31,32 @@
     mpv = {
       enable = true;
       defaultProfiles = ["gpu-hq"];
-      scripts = [pkgs.mpvScripts.mpris];
+      scripts = with pkgs.mpvScripts; [
+        mpris
+        encode
+        mpv-cheatsheet
+        uosc
+        thumbfast
+      ];
+      config = {
+        osd-bar = "no";
+        border = "no";
+      };
+      bindings = {
+        space = "cycle pause; script-binding uosc/flash-pause-indicator";
+        right = "seek 5; script-binding uosc/flash-timeline";
+        left = "seek -5; script-binding uosc/flash-timeline";
+        "shift+right" = "seek 30; script-binding uosc/flash-timeline";
+        "shift+left" = "seek -30; script-binding uosc/flash-timeline";
+        m = "no-osd cycle mute; script-binding uosc/flash-volume";
+        up = "no-osd add volume 10; script-binding uosc/flash-volume";
+        down = "no-osd add volume -10; script-binding uosc/flash-volume";
+        "[" = "no-osd add speed -0.1; script-binding uosc/flash-speed";
+        "]" = "no-osd add speed 0.1; script-binding uosc/flash-speed";
+        "\\" = "no-osd set speed 1; script-binding uosc/flash-speed";
+        ">" = "script-binding uosc/next; script-message-to uosc flash-elements top_bar,timeline";
+        "<" = "script-binding uosc/prev; script-message-to uosc flash-elements top_bar,timeline";
+      };
     };
   };
 
