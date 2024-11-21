5193a6d344c23e7e77dbd795185ffca2f65c3612
L-Trump <ltrump@163.com>
2024-11-21 16:06:48 +0800

    feat(hyprland): turn off eDP screen when laptap closed

--- hosts/matebook-gt14/home/hyprland.nix
+++ hosts/matebook-gt14/home/hyprland.nix
@@ -7,9 +7,10 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "eDP-1,preferred,0x0,auto"
-      "desc:Lenovo Group Limited LEN Y27q-20 0x01010101,preferred,auto-right,1.6"
-      "desc:Dell Inc. DELL SE2722H CVW0WP3,1920x1080@60,auto-right,auto"
-      ",preferred,auto-right,auto"
+      "desc:Lenovo Group Limited LEN Y27q-20 0x01010101,preferred,auto,1.6"
+      "desc:Dell Inc. DELL SE2722H CVW0WP3,1920x1080@60,1440x0,auto"
+      "desc:Dell Inc. DELL D2721H C1SSW53,1920x1080@60,3360x0,auto"
+      ",preferred,auto,auto"
     ];
     device = [
       {
@@ -23,5 +24,11 @@
       }
     ];
     exec-once = ["brightnessctl -d platform::micmute s 0"];
+    bindl = [
+      # trigger when the switch is turning on
+      '', switch:on:Lid Switch, exec, hyprctl keyword monitor "eDP-1, disable"''
+      # trigger when the switch is turning off
+      '', switch:off:Lid Switch, exec, hyprctl keyword monitor "eDP-1, preferred, 0x0, auto"''
+    ];
   };
 }
