3a7daffb13850635006aff44fb64e61f5900cbcc
L-Trump <ltrump@163.com>
2024-12-26 20:25:13 +0800

    fix(hosts/n100): proxmox-ve add ipaddress

--- hosts/n100/modules/proxmox/proxmox.nix
+++ hosts/n100/modules/proxmox/proxmox.nix
@@ -11,7 +11,10 @@ in {
   ];
   environment.systemPackages = with pkgs; [swtpm];
   environment.etc."swtpm_setup.conf".source = "${pkgs.swtpm}/etc/swtpm_setup.conf";
-  services.proxmox-ve.enable = true;
+  services.proxmox-ve = {
+    enable = true;
+    ipAddress = "192.168.2.111";
+  };
   nixpkgs.overlays = [
     inputs.proxmox-nixos.overlays.${system}
   ];
