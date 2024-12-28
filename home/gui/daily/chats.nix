d5e60d42ba2c8071bf3f4cd0493977571fbb6cd5
L-Trump <ltrump@163.com>
2024-12-28 16:04:09 +0800

    fix(package): fix qq desktopEntry

--- home/gui/daily/chats.nix
+++ home/gui/daily/chats.nix
@@ -7,7 +7,7 @@
 
   xdg.desktopEntries.qq = {
     name = "QQ";
-    exec = "${pkgs.qq}/bin/qq --enable-wayland-ime --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations";
+    exec = "${pkgs.qq}/bin/qq --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations";
     icon = "qq";
     settings.StartupWMClass = "QQ";
     categories = ["Chat" "Network"];
