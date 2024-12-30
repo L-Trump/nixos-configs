ce90a8060bb15c9a59ff7c8facc24bc526583616
L-Trump <ltrump@163.com>
2024-12-30 15:22:20 +0800

    fix(modules/desktop): clash-verge option tunMode depreciated

--- modules/nixos/desktop/network.nix
+++ modules/nixos/desktop/network.nix
@@ -3,15 +3,6 @@
   programs.clash-verge = {
     enable = true;
     autoStart = false;
-    tunMode = true;
     package = pkgs.clash-verge-rev;
   };
-  # systemd.services.clash-verge-rev = {
-  #   enable = true;
-  #   description = "Clash Verge Rev Service";
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.clash-verge-rev}/lib/clash-verge/resources/clash-verge-service";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  # };
 }
