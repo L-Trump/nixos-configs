a3b9bef6b873630820e38f169c083e9cb7ea4d1b
L-Trump <ltrump@163.com>
2025-01-10 22:17:41 +0800

    feat(hosts): add new host qfynat

---
+++ hosts/qfynat/modules/core.nix
@@ -0,0 +1,49 @@
+{myvars, lib, ...}: let
+  hostName = "qfynat";
+in {
+  # networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  services.openssh.enable = true;
+  services.resolved.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+
+  networking = {
+    inherit hostName;
+    usePredictableInterfaceNames = false;
+    domain = "localdomain";
+    nameservers = [
+      "8.8.8.8" # googledns
+      "8.8.4.4" # googledns
+    ];
+  };
+
+  nix.settings.substituters = lib.mkForce [
+    "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
+  ];
+
+  services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
--- vars/networking.nix
+++ vars/networking.nix
@@ -14,6 +14,7 @@ in rec {
     matebook-gt14.ipv4 = "10.144.144.250";
     n100.ipv4 = "10.144.144.111";
     tencent-vm-jp.ipv4 = "10.144.144.253";
+    qfynat.ipv4 = "10.144.144.252";
     dorm-router.ipv4 = "10.144.144.198";
     gs445.ipv4 = "10.144.144.195";
     oneplus-ace.ipv4 = "10.144.144.249";
