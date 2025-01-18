9b7d26d66825886469e58f5bc61d7724595ce276
L-Trump <ltrump@163.com>
2025-01-19 02:43:23 +0800

    feat(home): remove yazi file manager

--- home/core/yazi/default.nix
+++ home/core/yazi/default.nix
@@ -49,68 +49,70 @@
     )
     plugins;
 in {
-  home.packages = with pkgs; [xdragon];
-  programs.yazi = {
-    enable = true;
-    enableFishIntegration = false;
-    settings = {
-      manager.mouse_events = ["click" "scroll" "touch" "drag"];
-      opener.open = [
-        {
-          run = ''xdg-open "$1"'';
-          orphan = true;
-          desc = "Open";
-        }
-      ];
+  config = lib.mkIf false {
+    home.packages = with pkgs; [xdragon];
+    programs.yazi = {
+      enable = true;
+      enableFishIntegration = false;
+      settings = {
+        manager.mouse_events = ["click" "scroll" "touch" "drag"];
+        opener.open = [
+          {
+            run = ''xdg-open "$1"'';
+            orphan = true;
+            desc = "Open";
+          }
+        ];
+      };
+      keymap = {
+        manager.prepend_keymap =
+          [
+            {
+              on = "q";
+              run = "close";
+              desc = "Close the current tab; or quit if it is last tab";
+            }
+            {
+              on = "<C-c>";
+              run = "escape";
+              desc = "Exit visual mode; clear selected; or cancel search";
+            }
+            {
+              on = "!";
+              run = ''shell "$SHELL" --block --confirm'';
+              desc = "Open a shell here";
+            }
+            {
+              on = "e";
+              run = ''shell "''${EDITOR:-vi} \"$@\"" --block --confirm'';
+              desc = "Edit current by $EDITOR";
+            }
+            {
+              on = "<Enter>";
+              run = "plugin --sync smart-enter";
+              desc = "Enter/open the target";
+            }
+            {
+              on = "l";
+              run = "plugin --sync smart-enter";
+              desc = "Enter/open the target";
+            }
+            # { on = [ "n" "f" ]; run = "create"; desc = "Create a file"; }
+            # { on = [ "n" "d" ]; run = "create --dir"; desc = "Create a directory"; }
+          ]
+          ++ bookmark_keymaps
+          ++ plugin_keymaps;
+      };
+      initLua = ./yazi-init.lua;
+      plugins.smart-enter = ./plugin-smart-enter;
     };
-    keymap = {
-      manager.prepend_keymap =
-        [
-          {
-            on = "q";
-            run = "close";
-            desc = "Close the current tab; or quit if it is last tab";
-          }
-          {
-            on = "<C-c>";
-            run = "escape";
-            desc = "Exit visual mode; clear selected; or cancel search";
-          }
-          {
-            on = "!";
-            run = ''shell "$SHELL" --block --confirm'';
-            desc = "Open a shell here";
-          }
-          {
-            on = "e";
-            run = ''shell "''${EDITOR:-vi} \"$@\"" --block --confirm'';
-            desc = "Edit current by $EDITOR";
-          }
-          {
-            on = "<Enter>";
-            run = "plugin --sync smart-enter";
-            desc = "Enter/open the target";
-          }
-          {
-            on = "l";
-            run = "plugin --sync smart-enter";
-            desc = "Enter/open the target";
-          }
-          # { on = [ "n" "f" ]; run = "create"; desc = "Create a file"; }
-          # { on = [ "n" "d" ]; run = "create --dir"; desc = "Create a directory"; }
-        ]
-        ++ bookmark_keymaps
-        ++ plugin_keymaps;
-    };
-    initLua = ./yazi-init.lua;
-    plugins.smart-enter = ./plugin-smart-enter;
+
+    xdg.configFile."fish/functions/y.fish".source = ./y.fish;
+
+    programs.fish.interactiveShellInit = ''
+      if [ -n "$YAZI_ID" ]
+        trap 'ya pub dds-cd --str "$PWD"' EXIT
+      end
+    '';
   };
-
-  xdg.configFile."fish/functions/y.fish".source = ./y.fish;
-
-  programs.fish.interactiveShellInit = ''
-    if [ -n "$YAZI_ID" ]
-      trap 'ya pub dds-cd --str "$PWD"' EXIT
-    end
-  '';
 }
