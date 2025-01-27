e3f87dac098dc5b7cb03ddecf74450bec17cd577
L-Trump <ltrump@163.com>
2025-01-27 20:34:53 +0800

    chore(package): easytier and sunshine follows upstream

--- packages/default.nix
+++ packages/default.nix
@@ -23,15 +23,12 @@ in {
 
   sctgdesk-server = pkgs.callPackage ./sctgdesk-server {};
 
-  # TODO wait upstream merge https://nixpk.gs/pr-tracker.html?pr=375153
-  sunshine = pkgs.callPackage ./sunshine {};
-
   # Some package derived from unstable repo
   siyuan = pkgs-unstable.siyuan;
   # easytier = pkgs-unstable.easytier;
 
   # siyuan = pkgs.callPackage ./siyuan {};
-  easytier = pkgs.callPackage ./easytier {}; # TODO wait upstream merge https://nixpk.gs/pr-tracker.html?pr=376259
+  # easytier = pkgs.callPackage ./easytier {};
   # siyuan = pkgs.callPackage ./siyuan {};
   # linuxPackages_latest = pkgs.linuxPackages_latest.extend (_: _: {
   #   ipu6-drivers = pkgs.linuxPackages_latest.callPackage ./ipu6-drivers {};
@@ -39,4 +36,5 @@ in {
   # openvswitch = pkgs-stable.openvswitch.override {kernel = null;};
   # clouddrive2 = pkgs.callPackage ./clouddrive2 {};
   # snipaste = pkgs.callPackage ./snipaste {};
+  # sunshine = pkgs.callPackage ./sunshine {};
 }
