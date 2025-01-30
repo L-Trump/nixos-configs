163fdca3e38f674ef64d76bc3f99df9acdd4e4a7
L-Trump <ltrump@163.com>
2025-01-30 20:39:00 +0800

    feat(modules/server): allow caddy use plain configuration file

--- modules/nixos/server/caddy.nix
+++ modules/nixos/server/caddy.nix
@@ -1,15 +1,25 @@
 {
+  inputs,
   config,
   pkgs,
   lib,
   ...
 }: let
-  cfg.enable = builtins.hasAttr "caddyfile" config.age.secrets;
+  inherit (builtins) pathExists;
+  inherit (config.networking) hostName;
+  inherit (inputs) mysecrets;
+  plainCfgPath = "${mysecrets}/caddy/caddyfile-${hostName}";
+  cfg.enable =
+    lib.or (builtins.hasAttr "caddyfile" config.age.secrets) (pathExists plainCfgPath);
+  cfgPath =
+    if (builtins.hasAttr "caddyfile" config.age.secrets)
+    then config.age.secrets.caddyfile.path
+    else plainCfgPath;
 in {
   environment.systemPackages = lib.mkIf cfg.enable [pkgs.caddy];
 
   services.caddy = lib.mkIf cfg.enable {
     enable = true;
-    configFile = config.age.secrets.caddyfile.path;
+    configFile = cfgPath;
   };
 }
