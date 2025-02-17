0c0e4cab688a5b035ac4ccea0a58aa20c01b5014
L-Trump <ltrump@163.com>
2025-02-17 23:07:20 +0800

    feat(hosts): rog-ga502 ignore lidswitch off

--- hosts/rog-ga502/home/hyprland.nix
+++ hosts/rog-ga502/home/hyprland.nix
@@ -9,7 +9,7 @@
     monitor = lib.mkBefore [
       "eDP-1,preferred,0x0,1.333333"
       "desc:Lenovo Group Limited LEN Y27q-20 0x01010101,preferred,auto,1.6"
-      "desc:Dell Inc. DELL SE2722H CVW0WP3,1920x1080@60,1440x0,auto"
+      "desc:Dell Inc. DELL SE2722H CVW0WP3,1920x1080@60,1440x0,auto,vrr,0"
       "desc:Dell Inc. DELL D2721H C1SSW53,1920x1080@60,3360x0,auto"
       ",preferred,auto,auto"
     ];
--- hosts/rog-ga502/modules/core.nix
+++ hosts/rog-ga502/modules/core.nix
@@ -44,6 +44,8 @@ in {
     clinfo
   ];
 
+  services.logind.lidSwitch = "lock";
+
   # services.udev.extraRules = ''
   #   ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
   # '';
