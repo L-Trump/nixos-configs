6a0bd839958ddc69e57efb54220e5c6fb1f06475
L-Trump <ltrump@163.com>
2025-04-01 22:05:25 +0800

    feat(hosts/rog-ga502): hyprland add monitor

--- hosts/rog-ga502/home/hyprland.nix
+++ hosts/rog-ga502/home/hyprland.nix
@@ -11,6 +11,7 @@
       "desc:Lenovo Group Limited LEN Y27q-20 0x01010101,preferred,auto,1.6"
       "desc:Dell Inc. DELL SE2722H CVW0WP3,1920x1080@60,1440x0,auto,vrr,0"
       "desc:Dell Inc. DELL D2721H C1SSW53,1920x1080@60,3360x0,auto"
+      "desc:Dell Inc. DELL SE2722H H6RZVP3,1920x1080@60,3360x0,auto,vrr,0"
       ",preferred,auto,auto"
     ];
     bindl = [
