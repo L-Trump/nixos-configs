d4f27bc6694719ce60908031c5cfdb629c19030e
L-Trump <ltrump@163.com>
2025-04-08 01:15:06 +0800

    fix(modules/vpn): sjtu vpn cacert update

--- modules/nixos/desktop/vpn/default.nix
+++ modules/nixos/desktop/vpn/default.nix
@@ -9,4 +9,5 @@ in {
   ];
   environment.etc."ipsec.d/cacerts/isrgrootx1.pem".source = ./certs/isrgrootx1.pem;
   environment.etc."ipsec.d/cacerts/addtrust.pem".source = ./certs/addtrust.pem;
+  environment.etc."ipsec.d/cacerts/DigiCertGlobalRootG3.crt.pem".source = ./certs/DigiCertGlobalRootG3.crt.pem;
 }
