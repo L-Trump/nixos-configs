ddbb0b57000983f4db9fb780a4673c3c4e1fe35e
L-Trump <ltrump@163.com>
2025-04-25 00:54:02 +0800

    chore(containers): update

--- vars/containers.nix
+++ vars/containers.nix
@@ -1,15 +1,15 @@
 _: {
   nezha-server = {
     image = "registry.cn-shanghai.aliyuncs.com/naibahq/nezha-dashboard";
-    tag = "v1.10.2";
+    tag = "v1.12.0";
   };
   immich-machine-learning = {
     image = "ghcr.linkos.org/immich-app/immich-machine-learning";
-    tag = "v1.129.0";
+    tag = "v1.132.1";
     latestTag = "release";
   };
   siyuan-server = {
     image = "docker.linkos.org/b3log/siyuan";
-    tag = "v3.1.26";
+    tag = "v3.1.28";
   };
 }
