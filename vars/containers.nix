c3bd9a88a205becc9c1f1b0a4830a3017ef1b614
L-Trump <ltrump@163.com>
2025-10-08 22:06:18 +0800

    chore(container): update

--- vars/containers.nix
+++ vars/containers.nix
@@ -1,32 +1,32 @@
 _: {
   nezha-server = {
     image = "registry.cn-shanghai.aliyuncs.com/naibahq/nezha-dashboard";
-    digest = "sha256:a42ab1da824ed65bcf9ac26814aecb45acf678f6aa47de7c31f8f7784ed6f1ae";
+    digest = "sha256:531839f142a24ddd71de2fe09213182c2886aa2d324b9a501797046437715074";
   };
   immich-machine-learning = {
     image = "gh.mmm.fan/ghcr.io/immich-app/immich-machine-learning";
     latestTag = "release";
-    digest = "sha256:cc94659771d7e394d6406ebb0664069f2523062fda4f934def31648e903c4de2";
+    digest = "sha256:45626a33361ef7ed361de41b0d2dc19e5949442cdf0a8eb64b157dc8a04e9855";
   };
   siyuan-server = {
     image = "gh.mmm.fan/b3log/siyuan";
-    digest = "sha256:9888dcc44b84f9a0e8c03d037aec080a8dc6f767dffb10debc36092b02e6d1b0";
+    digest = "sha256:f83d07e1ebf00ba00a9e86876594a607e9b6833d770330df7b0ee29a415e8a1d";
   };
   cloudreve = {
     image = "gh.mmm.fan/cloudreve/cloudreve";
-    digest = "sha256:31a69b3654942ee7b6a8450e0f775c5e917b7105a199a9e261749c1b20189e3f";
+    digest = "sha256:a553e5138ffc039388a0b45124189d0dec08f0c9b90d57469dc6240df9bfa452";
   };
   cloudreve-redis = {
     image = "gh.mmm.fan/library/redis";
-    digest = "sha256:acb90ced0bd769b7c04cb4c32c4494ba7b3e0ee068bdbfff0eeb0d31c2a21078";
+    digest = "sha256:f0957bcaa75fd58a9a1847c1f07caf370579196259d69ac07f2e27b5b389b021";
   };
   cloudreve-postgresql = {
     image = "gh.mmm.fan/library/postgres";
-    digest = "sha256:0f4f20021a065d114083d1b95d9fb89ad847cbc4c3cc9238417815c7df42350f";
+    digest = "sha256:073e7c8b84e2197f94c8083634640ab37105effe1bc853ca4d5fbece3219b0e8";
   };
   rustdesk-api = {
     image = "gh.mmm.fan/lejianwen/rustdesk-api";
-    digest = "sha256:9493a5bfca63e8563457f36cdf66d8cec6cb9889016bb50a80145411b327e7c7";
+    digest = "sha256:ed35016339d3bcadf15c7bb3ae8490af1e3950c33f58fd2261ae009b94f5de45";
   };
   xpipe-webtop = {
     image = "gh.mmm.fan/ghcr.io/xpipe-io/xpipe-webtop";
