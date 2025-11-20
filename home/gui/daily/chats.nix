5df2f16f006d280ef68d868cb1a6c0cb85233280
L-Trump <ltrump@163.com>
2025-11-20 13:44:07 +0800

    fix(home/daily): fix qq entry

--- home/gui/daily/chats.nix
+++ home/gui/daily/chats.nix
@@ -6,15 +6,15 @@
     telegram-desktop
   ];
 
-  xdg.desktopEntries.qq = {
-    name = "QQ";
-    exec = "${pkgs.qq}/bin/qq --wayland-text-input-version=3";
-    icon = "qq";
-    settings.StartupWMClass = "QQ";
-    categories = [
-      "Chat"
-      "Network"
-    ];
-    comment = "QQ";
-  };
+  # xdg.desktopEntries.qq = {
+  #   name = "QQ";
+  #   exec = "${pkgs.qq}/bin/qq --wayland-text-input-version=3";
+  #   icon = "qq";
+  #   settings.StartupWMClass = "QQ";
+  #   categories = [
+  #     "Chat"
+  #     "Network"
+  #   ];
+  #   comment = "QQ";
+  # };
 }
