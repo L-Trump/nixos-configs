eeb924bf66ec13906e94b2aab88dc884dee33ea1
L-Trump <ltrump@163.com>
2026-02-03 16:58:35 +0800

    chore(container): update

--- vars/containers.nix
+++ vars/containers.nix
@@ -1,28 +1,28 @@
 _: {
   nezha-server = {
     image = "registry.cn-shanghai.aliyuncs.com/naibahq/nezha-dashboard";
-    digest = "sha256:c21078e4683300d29659f2011fc54ab9812e0065c60670843650bd377da0549c";
+    digest = "sha256:870adc32957498f23bf0a3554cab884a3505c83c77b7a4c5f8c862f3e62a2351";
   };
   immich-machine-learning = {
     image = "m.daocloud.io/ghcr.io/immich-app/immich-machine-learning";
     latestTag = "release";
-    digest = "sha256:b3deefd1826f113824e9d7bc30d905e7f823535887d03f869330946b6db3b44a";
+    digest = "sha256:531d2bccbe20d0412496e36455715a18d692911eca5e2ee37d32e1e4f50e14bb";
   };
   siyuan-server = {
     image = "m.daocloud.io/docker.io/b3log/siyuan";
-    digest = "sha256:bac87c146ebd11338be28b392971944bcf76a4c530e3ec2384eed5ceb37b9bf3";
+    digest = "sha256:d3981892e6e4021320a6da80f2c7e95e08759c355077bac57f3cf1cf4145ea2b";
   };
   cloudreve = {
     image = "m.daocloud.io/docker.io/cloudreve/cloudreve";
-    digest = "sha256:70debed86997389b00624d0ac2fe25ecbaca8dbe444157c6660bc93ca940cc9d";
+    digest = "sha256:6fc2945b016005d69de1bfd9c327c66a58362d69fc4ee662414f0e3ee1296a41";
   };
   cloudreve-redis = {
     image = "m.daocloud.io/docker.io/library/redis";
-    digest = "sha256:3906b477e4b60250660573105110c28bfce93b01243eab37610a484daebceb04";
+    digest = "sha256:73dad4271642c5966db88db7a7585fae7cf10b685d1e48006f31e0294c29fdd7";
   };
   cloudreve-postgresql = {
     image = "m.daocloud.io/docker.io/library/postgres";
-    digest = "sha256:38d5c9d522037d8bf0864c9068e4df2f8a60127c6489ab06f98fdeda535560f9";
+    digest = "sha256:5773fe724c49c42a7a9ca70202e11e1dff21fb7235b335a73f39297d200b73a2";
   };
   rustdesk-api = {
     image = "docker.linkos.org/lejianwen/rustdesk-api";
@@ -31,5 +31,9 @@ _: {
   xpipe-webtop = {
     image = "gh.qninq.cn/ghcr.io/xpipe-io/xpipe-webtop";
     digest = "sha256:b7198458c962a35ef360e1e68f6c3837169b64b607d13ef105a83b42ba20e70e";
+  };
+  ncm-api = {
+    image = "docker.linkos.org/moefurina/ncm-api";
+    digest = "sha256:7842091b794d24801d054d5b7cd47bd4fe8c12aed9dae0aa5f0fe76dc15463d8";
   };
 }
