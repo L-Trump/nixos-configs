5b994be9b30646614778917bc6f788eaba32bdf0
L-Trump <ltrump@163.com>
2026-03-20 14:26:41 +0800

    feat(nvidia): add nixos-cuda substituter

--- hosts/rog-ga502/modules/nvidia.nix
+++ hosts/rog-ga502/modules/nvidia.nix
@@ -32,6 +32,11 @@ in
   # ===============================================================================================
 
   # https://wiki.hyprland.org/Nvidia/
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
   environment.systemPackages = (lib.optionals config.programs.steam.enable [ steam-offload ]) ++ [
     pkgs.nvtopPackages.full
   ];
