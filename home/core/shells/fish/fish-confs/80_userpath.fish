456fda8e260b456b2f3778f56899b16a2b21f95a
L-Trump <ltrump@163.com>
2026-04-17 22:42:06 +0800

    chore(home/core): add .local/bin to PATH

--- home/core/shells/fish/fish-confs/80_userpath.fish
+++ home/core/shells/fish/fish-confs/80_userpath.fish
@@ -1,11 +1,12 @@
 set path_prepend \
-      $HOME/.npm/bin \
-      $HOME/.local/bin/custom_scripts \
-      $HOME/.cargo/bin \
-      $HOME/go/bin
+    $HOME/.npm/bin \
+    $HOME/.local/bin/custom_scripts \
+    $HOME/.local/bin \
+    $HOME/.cargo/bin \
+    $HOME/go/bin
 
 set path_append \
-      /opt/intelFPGA/20.1/modelsim_ase/bin
+    /opt/intelFPGA/20.1/modelsim_ase/bin
 
 if test (count $path_prepend) -gt 0
     fish_add_path -Pp $path_prepend
