a42d7b4fd5ee15ca2b06033a4953a15ed476eae4
L-Trump <ltrump@163.com>
2026-07-19 19:08:21 +0800

    fix(home/shells): fzf options migrate

--- home/core/shells/fzf.nix
+++ home/core/shells/fzf.nix
@@ -3,8 +3,9 @@
   programs.fzf = {
     enable = true;
     defaultCommand = "fd --type f";
-    changeDirWidgetCommand = "fd --type d";
-    fileWidgetCommand = "fd --type f";
+    changeDirWidget.command = "fd --type d";
+    fileWidget.command = "fd --type f";
+    historyWidget.command = "";
   };
 
   home.packages = [ pkgs.fd ];
