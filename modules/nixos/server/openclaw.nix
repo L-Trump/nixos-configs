68c9481612db80661699eafbe0025c5b555d9259
L-Trump <ltrump@163.com>
2026-08-15 14:29:52 +0800

    fix(openclaw): remove nix-openclaw garnix substituer

--- modules/nixos/server/openclaw.nix
+++ modules/nixos/server/openclaw.nix
@@ -25,10 +25,10 @@ in
       })
     ];
 
-    nix.settings = {
-      substituters = [ "https://cache.garnix.io" ];
-      trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-    };
+    # nix.settings = {
+    #   substituters = [ "https://cache.garnix.io" ];
+    #   trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+    # };
 
     environment.systemPackages = [
       pkgs.openclawPackages.openclaw
